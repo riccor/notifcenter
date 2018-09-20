ef6f5a8ca71e320ee68d5b25c1e5271d2213d42f
riccor <ricardocorreia_6@hotmail.com>
2018-09-20 14:35:54 +0100

    v30

--- src/main/java/pt/utl/ist/notifcenter/domain/Contacto.java
+++ src/main/java/pt/utl/ist/notifcenter/domain/Contacto.java
@@ -1,9 +1,37 @@
 package pt.utl.ist.notifcenter.domain;
 
+import org.fenixedu.bennu.core.domain.User;
+//import pt.ist.fenixframework.dml.runtime.DirectRelation;
+
 public class Contacto extends Contacto_Base {
-    
-    public Contacto() {
+
+    private org.fenixedu.bennu.core.domain.User utilizador;
+    private java.lang.String dados_contacto;
+    private Canal canal;
+
+    public Contacto(org.fenixedu.bennu.core.domain.User utilizador, java.lang.String dados_contacto, Canal canal) {
         super();
+
+        //DirectRelation<User, Contacto> relationUtilizadorTemContactos = getRelationUtilizadorTemContactos();
+        /// getUtilizador()        SERÁ QUE NAO PRECISAMOS de criar field 'utilizador'?!
+
+        setUtilizador(utilizador);
+        setDados_contacto(dados_contacto);
+        setCanal(canal);
     }
-    
+
+    @Override
+    public void setUtilizador(org.fenixedu.bennu.core.domain.User utilizador) {
+        this.utilizador = utilizador;
+    }
+
+    @Override
+    public void setDados_contacto(java.lang.String dados_contacto) {
+        this.dados_contacto = dados_contacto;
+    }
+
+    @Override
+    public void setCanal(pt.utl.ist.notifcenter.domain.Canal canal) {
+        this.canal = canal;
+    }
 }
--- src/main/webapp/WEB-INF/notifcenter/home.jsp
+++ src/main/webapp/WEB-INF/notifcenter/home.jsp
@@ -1,2 +1,2 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-Helooo v2.99 master ${world}!
+Helooo tv2.99 master ${world}!
