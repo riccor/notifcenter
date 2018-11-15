9728f653735801ea1b06dde33caa76dd56dec0a2
riccor <ricardocorreia_6@hotmail.com>
2018-11-15 01:24:55 +0000

    v3.87

--- src/main/java/pt/utl/ist/notifcenter/domain/NotifcenterException.java
+++ src/main/java/pt/utl/ist/notifcenter/domain/NotifcenterException.java
@@ -2,16 +2,7 @@ package pt.utl.ist.notifcenter.domain;
 
 //import org.fenixedu.bennu.core.domain.exceptions.BennuCoreDomainException;
 
-import com.google.gson.JsonObject;
-import org.springframework.http.HttpStatus;
-
 public class NotifcenterException extends RuntimeException {
-
-    /*final
-    private String[] args;
-    private String bundle;
-    private HttpStatus status;
-    */
 
     private ErrorsAndWarnings errorsAndWarnings;
     private String moreDetails;
@@ -28,27 +19,5 @@ public class NotifcenterException extends RuntimeException {
         this.errorsAndWarnings = errorsAndWarnings;
         this.moreDetails = moreDetails;
     }
-
-    /*
-    protected NotifcenterException(HttpStatus status, String bundle, String key, String... args) {
-        super();
-        this.status = status;
-        this.bundle = bundle;
-        this.key = key;
-        this.args = args;
-    }
-    */
-    //throw NotifcenterException.resourceNotFound();
-
-
-    /*
-    public NotifcenterException(Response.Status status, String key, String... args) {
-        super(status, key, args);
-    }
-
-    public static BennuCoreDomainException resourceNotFound(String id) {
-        return new BennuCoreDomainException(HttpStatus.NOT_FOUND, "e", id);
-    }
-    */
 
 }
