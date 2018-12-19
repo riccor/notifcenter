723eaa2bf40570eeb7fa8555decca45cb0a892fa
riccor <ricardocorreia_6@hotmail.com>
2018-12-19 05:34:04 +0000

    v4.21.2 (sendmessage2 - json support for sendmessage - fixed)

--- src/main/java/pt/utl/ist/notifcenter/domain/TwilioWhatsapp.java
+++ src/main/java/pt/utl/ist/notifcenter/domain/TwilioWhatsapp.java
@@ -96,7 +96,7 @@ public class TwilioWhatsapp extends TwilioWhatsapp_Base {
         body.put("To", Arrays.asList("initializing...")); ///
         body.put("From", Arrays.asList(this.getFromPhoneNumber()));
 
-        String linkForMessage = " Check " + NotifcenterSpringConfiguration.getConfiguration().notifcenterUrl() + "/notifcenter/" + this.getExternalId() + "/view";
+        String linkForMessage = " Check " + NotifcenterSpringConfiguration.getConfiguration().notifcenterUrl() + "/notifcenter/" + msg.getExternalId() + "/view";
         String message = msg.getTextoCurto() + linkForMessage;
         body.put("Body", Arrays.asList(message));
 
